cc670688a6e3e9d729f225f43029bd04b509e82b
Anyanwu-Ebere Chiemekam <aceanyanwu02@gmail.com>
2025-06-15 22:21:17 +0100

    feat : commented unfinished method

--- application/controller/devAPI/auth.go
+++ application/controller/devAPI/auth.go
@@ -1,17 +1,13 @@
 package public_controller
 
 import (
-	"net/http"
-
 	"gateman.io/application/interfaces"
-	application_usecase "gateman.io/application/usecases/application"
-	server_response "gateman.io/infrastructure/serverResponse"
 )
 
 func APIVerifyAuthToken(ctx *interfaces.ApplicationContext[any]) {
-	_, err := application_usecase.VerifyAuthTokenUseCase(ctx.Ctx, ctx.Param["token"].(string))
-	if err != nil {
-		return
-	}
-	server_response.Responder.Respond(ctx.Ctx, http.StatusOK, "token verified", nil, nil, nil)
+	//	 err := application_usecase.VerifyAuthTokenUseCase(ctx.Ctx, ctx.Param["token"].(string),ctx.Param["appID"].(string) )
+	//		if err != nil {
+	//			return
+	//		}
+	//		server_response.Responder.Respond(ctx.Ctx, http.StatusOK, "token verified", nil, nil, nil)
 }
